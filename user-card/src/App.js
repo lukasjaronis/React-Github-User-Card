5419cbc6fc685f647e105c95ea59354b2b58aa5c
lukasjaronis <jaronisluke13@gmail.com>
2019-12-03 15:41:13 -0500

    init

---
+++ user-card/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import GitCard from './Components/GitCards';
+
+function App() {
+  return (
+    <div className="App">
+     
+<GitCard />
+
+    </div>
+  );
+}
+
+export default App;
