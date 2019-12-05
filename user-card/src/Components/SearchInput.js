f1ca10f8b8f11d4bb921959ce65bff40d82ed730
lukasjaronis <jaronisluke13@gmail.com>
2019-12-05 10:52:39 -0500

    added styling to app header

--- user-card/src/Components/SearchInput.js
+++ user-card/src/Components/SearchInput.js
@@ -1,4 +1,52 @@
 import React, { Component } from "react";
+import styled from 'styled-components';
+
+const AppHeaderStyles = styled.div` 
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 1rem;
+
+
+form {
+    input {
+        border-top: none;
+        border-right: none;
+        border-left: none;
+        border-bottom: 2px solid #66fcf1;
+        padding: 5px;
+        background: none;
+        color: #66fcf1;
+        letter-spacing: 2px;
+
+        &::placeholder {
+            color: #66fcf1;
+        }
+
+        &:focus {
+          outline: none;
+          border-top: 2px solid #45A29E;
+        }
+    }
+
+    button {
+        margin: 1rem;
+        background: none;
+        border: 1px solid #66fcf1;
+        padding: 5px;
+        color: #fff;
+
+        &:hover {
+           border: 1px solid #1F2833;
+           background: #66fcf1;
+           color: #1F2833;
+        }
+      
+    }
+}
+
+`;
 
 class AppHeader extends Component {
   state = {
@@ -17,6 +65,7 @@ class AppHeader extends Component {
 
   render() {
     return (
+        <AppHeaderStyles>
       <form onSubmit={this.handleSubmit}>
         <input
           type="text"
@@ -26,6 +75,7 @@ class AppHeader extends Component {
         />
         <button type="submit">Search</button>
       </form>
+      </AppHeaderStyles>
     );
   }
 }
